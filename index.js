fb8c9d6863db0dbb055d4441375660f0a3befe54
TFCarlos <carlos@tufactoria.com>
2020-11-11 13:09:01 +0100

    FINAL FINAL FINAL

--- index.js
+++ index.js
@@ -66,328 +66,325 @@ const colorList = [
 // [div1_1][div1_2][button1_1][button1_2]
 // [div2_1][div2_2][button2_1][button2_2]
 
-createList();
-function createList() {
-  let pointer = document.querySelector(".color-list");
-
-  // 1º row PAIR.
-  let div1_1 = document.createElement("DIV");
-  div1_1.classList.add("color-name");
-  div1_1.innerHTML = "Color: white";
-
-  let div1_2 = document.createElement("DIV");
-  div1_2.classList.add("color-show");
-  div1_2.innerHTML = "Muestra";
-  div1_2.style.backgroundColor = "White";
-
-  let button1_1 = document.createElement("BUTTON");
-  button1_1.classList.add("color-set");
-  button1_1.innerHTML = "Next item color";
-
-  let button1_2 = document.createElement("BUTTON");
-  button1_2.classList.add("color-set");
-  button1_2.innerHTML = "Page color";
-
-  let li1 = document.createElement("LI");
-  li1.classList.add("color-item");
-
-  li1.appendChild(div1_1);
-  li1.appendChild(div1_2);
-  li1.appendChild(button1_1);
-  li1.appendChild(button1_2);
-
-  pointer.appendChild(li1);
-
-  // 2º row ODD.
-  let div2_1 = document.createElement("DIV");
-  div2_1.classList.add("color-name");
-  div2_1.innerHTML = "Color: red";
-
-  let div2_2 = document.createElement("DIV");
-  div2_2.classList.add("color-show");
-  div2_2.innerHTML = "Muestra";
-  div2_2.style.backgroundColor = "Red";
-
-  let button2_1 = document.createElement("BUTTON");
-  button2_1.classList.add("color-set");
-  button2_1.innerHTML = "Next item color";
-
-  let button2_2 = document.createElement("BUTTON");
-  button2_2.classList.add("color-set");
-  button2_2.innerHTML = "Page color";
-
-  let li2 = document.createElement("LI");
-  li2.classList.add("color-item");
-
-  li2.appendChild(div2_1);
-  li2.appendChild(div2_2);
-  li2.appendChild(button2_1);
-  li2.appendChild(button2_2);
-
-  pointer.appendChild(li2);
-
-  // 3º row PAIR.
-  let div3_1 = document.createElement("DIV");
-  div3_1.classList.add("color-name");
-  div3_1.innerHTML = "Color: orange";
-
-  let div3_2 = document.createElement("DIV");
-  div3_2.classList.add("color-show");
-  div3_2.innerHTML = "Muestra";
-  div3_2.style.backgroundColor = "Orange";
-
-  let button3_1 = document.createElement("BUTTON");
-  button3_1.classList.add("color-set");
-  button3_1.innerHTML = "Next item color";
-
-  let button3_2 = document.createElement("BUTTON");
-  button3_2.classList.add("color-set");
-  button3_2.innerHTML = "Page color";
-
-  let li3 = document.createElement("LI");
-  li3.classList.add("color-item");
-
-  li3.appendChild(div3_1);
-  li3.appendChild(div3_2);
-  li3.appendChild(button3_1);
-  li3.appendChild(button3_2);
-
-  pointer.appendChild(li3);
-
-  // 4º row ODD.
-  let div4_1 = document.createElement("DIV");
-  div4_1.classList.add("color-name");
-  div4_1.innerHTML = "Color: yellow";
-
-  let div4_2 = document.createElement("DIV");
-  div4_2.classList.add("color-show");
-  div4_2.innerHTML = "Muestra";
-  div4_2.style.backgroundColor = "Yellow";
-
-  let button4_1 = document.createElement("BUTTON");
-  button4_1.classList.add("color-set");
-  button4_1.innerHTML = "Next item color";
+let pointer = document.querySelector(".color-list");
+
+// 1º row PAIR.
+let div1_1 = document.createElement("DIV");
+div1_1.classList.add("color-name");
+div1_1.innerHTML = "Color: white";
+
+let div1_2 = document.createElement("DIV");
+div1_2.classList.add("color-show");
+div1_2.innerHTML = "Muestra";
+div1_2.style.backgroundColor = "White";
+
+let button1_1 = document.createElement("BUTTON");
+button1_1.classList.add("color-set");
+button1_1.innerHTML = "Next item color";
 
-  let button4_2 = document.createElement("BUTTON");
-  button4_2.classList.add("color-set");
-  button4_2.innerHTML = "Page color";
+let button1_2 = document.createElement("BUTTON");
+button1_2.classList.add("color-set");
+button1_2.innerHTML = "Page color";
 
-  let li4 = document.createElement("LI");
-  li4.classList.add("color-item");
+let li1 = document.createElement("LI");
+li1.classList.add("color-item");
+
+li1.appendChild(div1_1);
+li1.appendChild(div1_2);
+li1.appendChild(button1_1);
+li1.appendChild(button1_2);
+
+pointer.appendChild(li1);
+
+// 2º row ODD.
+let div2_1 = document.createElement("DIV");
+div2_1.classList.add("color-name");
+div2_1.innerHTML = "Color: red";
+
+let div2_2 = document.createElement("DIV");
+div2_2.classList.add("color-show");
+div2_2.innerHTML = "Muestra";
+div2_2.style.backgroundColor = "Red";
+
+let button2_1 = document.createElement("BUTTON");
+button2_1.classList.add("color-set");
+button2_1.innerHTML = "Next item color";
 
-  li4.appendChild(div4_1);
-  li4.appendChild(div4_2);
-  li4.appendChild(button4_1);
-  li4.appendChild(button4_2);
-
-  pointer.appendChild(li4);
+let button2_2 = document.createElement("BUTTON");
+button2_2.classList.add("color-set");
+button2_2.innerHTML = "Page color";
 
-  // 5º row PAIR.
-  let div5_1 = document.createElement("DIV");
-  div5_1.classList.add("color-name");
-  div5_1.innerHTML = "Color: orchid";
+let li2 = document.createElement("LI");
+li2.classList.add("color-item");
+
+li2.appendChild(div2_1);
+li2.appendChild(div2_2);
+li2.appendChild(button2_1);
+li2.appendChild(button2_2);
+
+pointer.appendChild(li2);
+
+// 3º row PAIR.
+let div3_1 = document.createElement("DIV");
+div3_1.classList.add("color-name");
+div3_1.innerHTML = "Color: orange";
+
+let div3_2 = document.createElement("DIV");
+div3_2.classList.add("color-show");
+div3_2.innerHTML = "Muestra";
+div3_2.style.backgroundColor = "Orange";
+
+let button3_1 = document.createElement("BUTTON");
+button3_1.classList.add("color-set");
+button3_1.innerHTML = "Next item color";
 
-  let div5_2 = document.createElement("DIV");
-  div5_2.classList.add("color-show");
-  div5_2.innerHTML = "Muestra";
-  div5_2.style.backgroundColor = "Orchid";
-
-  let button5_1 = document.createElement("BUTTON");
-  button5_1.classList.add("color-set");
-  button5_1.innerHTML = "Next item color";
+let button3_2 = document.createElement("BUTTON");
+button3_2.classList.add("color-set");
+button3_2.innerHTML = "Page color";
 
-  let button5_2 = document.createElement("BUTTON");
-  button5_2.classList.add("color-set");
-  button5_2.innerHTML = "Page color";
+let li3 = document.createElement("LI");
+li3.classList.add("color-item");
+
+li3.appendChild(div3_1);
+li3.appendChild(div3_2);
+li3.appendChild(button3_1);
+li3.appendChild(button3_2);
+
+pointer.appendChild(li3);
+
+// 4º row ODD.
+let div4_1 = document.createElement("DIV");
+div4_1.classList.add("color-name");
+div4_1.innerHTML = "Color: yellow";
+
+let div4_2 = document.createElement("DIV");
+div4_2.classList.add("color-show");
+div4_2.innerHTML = "Muestra";
+div4_2.style.backgroundColor = "Yellow";
+
+let button4_1 = document.createElement("BUTTON");
+button4_1.classList.add("color-set");
+button4_1.innerHTML = "Next item color";
 
-  let li5 = document.createElement("LI");
-  li5.classList.add("color-item");
+let button4_2 = document.createElement("BUTTON");
+button4_2.classList.add("color-set");
+button4_2.innerHTML = "Page color";
 
-  li5.appendChild(div5_1);
-  li5.appendChild(div5_2);
-  li5.appendChild(button5_1);
-  li5.appendChild(button5_2);
+let li4 = document.createElement("LI");
+li4.classList.add("color-item");
+
+li4.appendChild(div4_1);
+li4.appendChild(div4_2);
+li4.appendChild(button4_1);
+li4.appendChild(button4_2);
+
+pointer.appendChild(li4);
+
+// 5º row PAIR.
+let div5_1 = document.createElement("DIV");
+div5_1.classList.add("color-name");
+div5_1.innerHTML = "Color: orchid";
+
+let div5_2 = document.createElement("DIV");
+div5_2.classList.add("color-show");
+div5_2.innerHTML = "Muestra";
+div5_2.style.backgroundColor = "Orchid";
+
+let button5_1 = document.createElement("BUTTON");
+button5_1.classList.add("color-set");
+button5_1.innerHTML = "Next item color";
 
-  pointer.appendChild(li5);
+let button5_2 = document.createElement("BUTTON");
+button5_2.classList.add("color-set");
+button5_2.innerHTML = "Page color";
 
-  // 6º row ODD.
-  let div6_1 = document.createElement("DIV");
-  div6_1.classList.add("color-name");
-  div6_1.innerHTML = "Color: pink";
+let li5 = document.createElement("LI");
+li5.classList.add("color-item");
 
-  let div6_2 = document.createElement("DIV");
-  div6_2.classList.add("color-show");
-  div6_2.innerHTML = "Muestra";
-  div6_2.style.backgroundColor = "Pink";
+li5.appendChild(div5_1);
+li5.appendChild(div5_2);
+li5.appendChild(button5_1);
+li5.appendChild(button5_2);
 
-  let button6_1 = document.createElement("BUTTON");
-  button6_1.classList.add("color-set");
-  button6_1.innerHTML = "Next item color";
+pointer.appendChild(li5);
 
-  let button6_2 = document.createElement("BUTTON");
-  button6_2.classList.add("color-set");
-  button6_2.innerHTML = "Page color";
+// 6º row ODD.
+let div6_1 = document.createElement("DIV");
+div6_1.classList.add("color-name");
+div6_1.innerHTML = "Color: pink";
 
-  let li6 = document.createElement("LI");
-  li6.classList.add("color-item");
+let div6_2 = document.createElement("DIV");
+div6_2.classList.add("color-show");
+div6_2.innerHTML = "Muestra";
+div6_2.style.backgroundColor = "Pink";
 
-  li6.appendChild(div6_1);
-  li6.appendChild(div6_2);
-  li6.appendChild(button6_1);
-  li6.appendChild(button6_2);
+let button6_1 = document.createElement("BUTTON");
+button6_1.classList.add("color-set");
+button6_1.innerHTML = "Next item color";
 
-  pointer.appendChild(li6);
+let button6_2 = document.createElement("BUTTON");
+button6_2.classList.add("color-set");
+button6_2.innerHTML = "Page color";
 
-  // 7º row PAIR.
-  let div7_1 = document.createElement("DIV");
-  div7_1.classList.add("color-name");
-  div7_1.innerHTML = "Color: green";
-
-  let div7_2 = document.createElement("DIV");
-  div7_2.classList.add("color-show");
-  div7_2.innerHTML = "Muestra";
-  div7_2.style.backgroundColor = "Green";
-
-  let button7_1 = document.createElement("BUTTON");
-  button7_1.classList.add("color-set");
-  button7_1.innerHTML = "Next item color";
-
-  let button7_2 = document.createElement("BUTTON");
-  button7_2.classList.add("color-set");
-  button7_2.innerHTML = "Page color";
-
-  let li7 = document.createElement("LI");
-  li7.classList.add("color-item");
-
-  li7.appendChild(div7_1);
-  li7.appendChild(div7_2);
-  li7.appendChild(button7_1);
-  li7.appendChild(button7_2);
-
-  pointer.appendChild(li7);
-
-  //8º row ODD.
-  let div8_1 = document.createElement("DIV");
-  div8_1.classList.add("color-name");
-  div8_1.innerHTML = "Color: silver";
-
-  let div8_2 = document.createElement("DIV");
-  div8_2.classList.add("color-show");
-  div8_2.innerHTML = "Muestra";
-  div8_2.style.backgroundColor = "Silver";
-
-  let button8_1 = document.createElement("BUTTON");
-  button8_1.classList.add("color-set");
-  button8_1.innerHTML = "Next item color";
-
-  let button8_2 = document.createElement("BUTTON");
-  button8_2.classList.add("color-set");
-  button8_2.innerHTML = "Page color";
-
-  let li8 = document.createElement("LI");
-  li8.classList.add("color-item");
-
-  li8.appendChild(div8_1);
-  li8.appendChild(div8_2);
-  li8.appendChild(button8_1);
-  li8.appendChild(button8_2);
-
-  pointer.appendChild(li8);
-
-  // Color alert.
-
-  div1_1.addEventListener("click", function(alertaWhite) {
-    alert("White");
-  });
-
-  div2_1.addEventListener("click", function(alertRed) {
-    alert("Red");
-  });
-
-  div3_1.addEventListener("click", function(alertaOrange) {
-    alert("Orange");
-  });
-
-  div4_1.addEventListener("click", function(alertaYellow) {
-    alert("Yellow");
-  });
-
-  div5_1.addEventListener("click", function(alertaOrchid) {
-    alert("Orchid");
-  });
-
-  div6_1.addEventListener("click", function(alertaPink) {
-    alert("Pink");
-  });
-
-  div7_1.addEventListener("click", function(alertaGreen) {
-    alert("Green");
-  });
-
-  div8_1.addEventListener("click", function(alertaSilver) {
-    alert("Silver");
-  });
-
-  // ODD background. [2 4 6 8]
-  li2.classList.add("color-item--odd");
-  li4.classList.add("color-item--odd");
-  li6.classList.add("color-item--odd");
-  li8.classList.add("color-item--odd");
-
-  // Set next item color.
-  button1_1.addEventListener("click", function(alertaSetWhite) {
-    li2.style.backgroundColor = "White";
-  });
-  button2_1.addEventListener("click", function(alertaSetRed) {
-    li3.style.backgroundColor = "Red";
-  });
-  button3_1.addEventListener("click", function(alertaSetOrange) {
-    li4.style.backgroundColor = "Orange";
-  });
-  button4_1.addEventListener("click", function(alertaSetYellow) {
-    li5.style.backgroundColor = "Yellow";
-  });
-  button5_1.addEventListener("click", function(alertaSetOrchid) {
-    li6.style.backgroundColor = "Orchid";
-  });
-  button6_1.addEventListener("click", function(alertaSetPink) {
-    li7.style.backgroundColor = "Pink";
-  });
-  button7_1.addEventListener("click", function(alertaSetGreen) {
-    li8.style.backgroundColor = "Green";
-  });
-  button8_1.addEventListener("click", function(alertaSetSilver) {
-    li1.style.backgroundColor = "Silver";
-  });
-
-  // Set body background colorList.
-  let bodyBack = document.querySelector(".color-list").parentNode;
-
-  button1_2.addEventListener("click", function(backSetWhite) {
-    bodyBack.style.backgroundColor = "White";
-  });
-  button2_2.addEventListener("click", function(backSetRed) {
-    bodyBack.style.backgroundColor = "Red";
-  });
-  button3_2.addEventListener("click", function(backSetOrange) {
-    bodyBack.style.backgroundColor = "Orange";
-  });
-  button4_2.addEventListener("click", function(backSetYellow) {
-    bodyBack.style.backgroundColor = "Yellow";
-  });
-  button5_2.addEventListener("click", function(backSetOrchid) {
-    bodyBack.style.backgroundColor = "Orchid";
-  });
-  button6_2.addEventListener("click", function(backSetPink) {
-    bodyBack.style.backgroundColor = "Pink";
-  });
-  button7_2.addEventListener("click", function(backSetGreen) {
-    bodyBack.style.backgroundColor = "Green";
-  });
-  button8_2.addEventListener("click", function(backSetSilver) {
-    bodyBack.style.backgroundColor = "Silver";
-  });
-}
+let li6 = document.createElement("LI");
+li6.classList.add("color-item");
+
+li6.appendChild(div6_1);
+li6.appendChild(div6_2);
+li6.appendChild(button6_1);
+li6.appendChild(button6_2);
+
+pointer.appendChild(li6);
+
+// 7º row PAIR.
+let div7_1 = document.createElement("DIV");
+div7_1.classList.add("color-name");
+div7_1.innerHTML = "Color: green";
+
+let div7_2 = document.createElement("DIV");
+div7_2.classList.add("color-show");
+div7_2.innerHTML = "Muestra";
+div7_2.style.backgroundColor = "Green";
+
+let button7_1 = document.createElement("BUTTON");
+button7_1.classList.add("color-set");
+button7_1.innerHTML = "Next item color";
+
+let button7_2 = document.createElement("BUTTON");
+button7_2.classList.add("color-set");
+button7_2.innerHTML = "Page color";
+
+let li7 = document.createElement("LI");
+li7.classList.add("color-item");
+
+li7.appendChild(div7_1);
+li7.appendChild(div7_2);
+li7.appendChild(button7_1);
+li7.appendChild(button7_2);
+
+pointer.appendChild(li7);
+
+// 8º row ODD.
+let div8_1 = document.createElement("DIV");
+div8_1.classList.add("color-name");
+div8_1.innerHTML = "Color: silver";
+
+let div8_2 = document.createElement("DIV");
+div8_2.classList.add("color-show");
+div8_2.innerHTML = "Muestra";
+div8_2.style.backgroundColor = "Silver";
+
+let button8_1 = document.createElement("BUTTON");
+button8_1.classList.add("color-set");
+button8_1.innerHTML = "Next item color";
+
+let button8_2 = document.createElement("BUTTON");
+button8_2.classList.add("color-set");
+button8_2.innerHTML = "Page color";
+
+let li8 = document.createElement("LI");
+li8.classList.add("color-item");
+
+li8.appendChild(div8_1);
+li8.appendChild(div8_2);
+li8.appendChild(button8_1);
+li8.appendChild(button8_2);
+
+pointer.appendChild(li8);
+
+// Color alert.
+
+div1_1.addEventListener("click", function(alertaWhite) {
+  alert("White");
+});
+
+div2_1.addEventListener("click", function(alertRed) {
+  alert("Red");
+});
+
+div3_1.addEventListener("click", function(alertaOrange) {
+  alert("Orange");
+});
+
+div4_1.addEventListener("click", function(alertaYellow) {
+  alert("Yellow");
+});
+
+div5_1.addEventListener("click", function(alertaOrchid) {
+  alert("Orchid");
+});
+
+div6_1.addEventListener("click", function(alertaPink) {
+  alert("Pink");
+});
+
+div7_1.addEventListener("click", function(alertaGreen) {
+  alert("Green");
+});
+
+div8_1.addEventListener("click", function(alertaSilver) {
+  alert("Silver");
+});
+
+// ODD background. [2 4 6 8]
+li2.classList.add("color-item--odd");
+li4.classList.add("color-item--odd");
+li6.classList.add("color-item--odd");
+li8.classList.add("color-item--odd");
+
+// Set next item color.
+button1_1.addEventListener("click", function(alertaSetWhite) {
+  li2.style.backgroundColor = "White";
+});
+button2_1.addEventListener("click", function(alertaSetRed) {
+  li3.style.backgroundColor = "Red";
+});
+button3_1.addEventListener("click", function(alertaSetOrange) {
+  li4.style.backgroundColor = "Orange";
+});
+button4_1.addEventListener("click", function(alertaSetYellow) {
+  li5.style.backgroundColor = "Yellow";
+});
+button5_1.addEventListener("click", function(alertaSetOrchid) {
+  li6.style.backgroundColor = "Orchid";
+});
+button6_1.addEventListener("click", function(alertaSetPink) {
+  li7.style.backgroundColor = "Pink";
+});
+button7_1.addEventListener("click", function(alertaSetGreen) {
+  li8.style.backgroundColor = "Green";
+});
+button8_1.addEventListener("click", function(alertaSetSilver) {
+  li1.style.backgroundColor = "Silver";
+});
+
+// Set body background colorList.
+let bodyBack = document.querySelector(".color-list").parentNode;
+
+button1_2.addEventListener("click", function(backSetWhite) {
+  bodyBack.style.backgroundColor = "White";
+});
+button2_2.addEventListener("click", function(backSetRed) {
+  bodyBack.style.backgroundColor = "Red";
+});
+button3_2.addEventListener("click", function(backSetOrange) {
+  bodyBack.style.backgroundColor = "Orange";
+});
+button4_2.addEventListener("click", function(backSetYellow) {
+  bodyBack.style.backgroundColor = "Yellow";
+});
+button5_2.addEventListener("click", function(backSetOrchid) {
+  bodyBack.style.backgroundColor = "Orchid";
+});
+button6_2.addEventListener("click", function(backSetPink) {
+  bodyBack.style.backgroundColor = "Pink";
+});
+button7_2.addEventListener("click", function(backSetGreen) {
+  bodyBack.style.backgroundColor = "Green";
+});
+button8_2.addEventListener("click", function(backSetSilver) {
+  bodyBack.style.backgroundColor = "Silver";
+});
 
 // Click body background.
 let bodyClick = document.getElementsByTagName("body")[0];
